637da79f0d7de9d0662370b13c1d840a5df50462
Mikael Andersson <andersson.mikael@siemens.com>
2024-02-26 00:11:17 +0100

    Västerbotten verkar funka

--- scripts/kml2gpx.py
+++ scripts/kml2gpx.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import os,sys
 import argparse
-import xml
-from xml.dom import minidom
-import xml.etree.ElementTree as gfg  
-from xml.etree.ElementTree import Element, SubElement, ElementTree, tostring
+#import xml
+#from xml.dom import minidom
+#import xml.etree.ElementTree as ET  
+#from xml.etree.ElementTree import Element, SubElement, ElementTree, tostring
 import subprocess
-
+import gpxpy
+import gpxpy.gpx
 
 
 
@@ -39,55 +40,38 @@ for line in filedata:
             line=line.replace("0.0 ",'')
             db[name]=line
             #print("DBG:"+line)
-outdata=[]
-#root = gfg.Element('gpx version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"')
-root = gfg.Element('<gpx version="1.1" creator="GDAL 2.2.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ogr="http://osgeo.org/gdal" xmlns="http://www.topografix.com/GPX/1/1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">')
 
+gpx = gpxpy.gpx.GPX()
 for n in db:
-    track=gfg.Element("trk")
-    name=gfg.SubElement(track,"name")
-    name.text=n
-    trackseq=gfg.Element("trkseq")
-    root.append(track)
-    track.append(trackseq)
+    gpx_track = gpxpy.gpx.GPXTrack(name=n)
+    gpx.tracks.append(gpx_track)
+    # Segment
+    gpx_segment = gpxpy.gpx.GPXTrackSegment()
+    gpx_track.segments.append(gpx_segment)
     c=db[n]
     i=0;
     pos=list(c.split(","))
     #Remove last 0.0 to get an even list
     pos.pop()
     psize=len(pos)
-#    print("DBG:"+str(psize))
     for i in range(0,psize,2):
         p=pos[i]
         p=p.strip()
-        point="trkpt"
-        point=point+" lon=\""+str(pos[i])+"\""+ " lat=\""+str(pos[i+1])+"\""
-        i=i+2
-#        print("DBG:"+str(i)+" "+point)
-        trackpoint=gfg.SubElement(trackseq,point)
-    outdata.append(track)
-    tree = gfg.ElementTree(track)
-    NewXML="tmp."+str(n)+".gpx"
-    out = open(NewXML, 'wb')
-    out.write(b'<?xml version="1.0"?>')
-    out.write(b'<gpx version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">')
-    tree.write(out, encoding = 'UTF-8', xml_declaration = False)
-    #    with open ("tmp."+str(n)+".gpx", "wb") as files : 
-#       tree.write(files) 
+        lon=pos[i]
+        lat=pos[i+1]
+        gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat,lon))
 
+        
+outfile=open("all.gpx","w")
+for line in gpx.to_xml():
+    outfile.write(line)
+outfile.close()
     
-with open('tmp.all.gpx', 'w') as outfile:
-    for n in db:
-        fname="tmp."+str(n)+".gpx"
-        cmd="echo <gpx> >> "+fname
-        returned_value = subprocess.call(cmd, shell=True)
-        dest=str(n)+".gpx"
-        print("Prettyfing "+fname+" to "+dest)
-        cmd="xmllint --format "+fname+ ">" +dest
-        returned_value = subprocess.call(cmd, shell=True)
-        
-        
 
-print("Removing temporary files")
-cmd="rm -f tmp.*.gpx"
-#returned_value = subprocess.call(cmd, shell=True)
+
+#print('Created GPX:', gpx.to_xml())
+
+
+
+
+ 
