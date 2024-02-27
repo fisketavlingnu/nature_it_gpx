2f013cfea7387e2c4c67412490453c0f1e4656f4
Mikael Andersson <andersson.mikael@siemens.com>
2024-02-27 12:17:39 +0100

    Struktur tillsnyggad

--- README.md
+++ README.md
@@ -9,10 +9,48 @@ Vi tar inget ansvar för att kartorna är korrekta. Ni använder dessa gpx filer
 GPX filerna är skapade utifrån länsstyrelsen originaldata men är inte på något sätt synkade eller 
 kvalitetssäkrade. Det innebär att ni använder kartorna helt på egen risk!
 
+## Struktur ##
+Då enligt https://support.garmin.com/sv-SE/?faq=HEOCUiTXDo341NvsKwrrn9
+garmin stödjer max 200 "spår" har jag delat upp alla gpx filer i olika områden.
+Det underlättar även användningen i Garmin Basecamp.
+
+OBS:Om man tex laddar ner kompletta västerbotten så är det 182 spår. Man får ingen varning om man överskrider
+antalet spår i sin navigator så därför rekommenderar vi att man väljer de områden man ska till
+och rensar spår som man inte ska till (just nu)
+
+### Västerbotten ###
+Här finns alla gpx filer under vasterbotten/gpx
+* Vilhelmina Södra: Alla områden som har ett namn som startar på VS (Vilhelmina_Sodra_VS.gpx)
+* Vilhelmina Norra: Alla områden som har ett namn som startar på VN (Vilhelmina_Norra_VN.gpx)
+* Storuman Ubmeje: Alla områden som har ett namn som startar på U (Storuman_Ubmeje_U.gpx) 
+* Storuman Vapsten: Alla områden som har ett namn som startar på VA (Storuman_Vapsten_VA.gpx) 
+* Sorsele Ran: Alla områden som har ett namn som startar på R (Sorsele_Ran_R.gpx) 
+* Sorsele Gran: Alla områden som har ett namn som startar på G (Sorsele_Gran_G.gpx)
+
+Sen finns en fil som innehåller alla områden för västerbotten (hela_vasterbotten.gpx).
+Den filen kan med fördel användas i tex Wehunt om man vill ha in alla områden (Wehunt verkar inte ha några begränsningar här)
+
 ## Kommande ##
 Jag ska försöka ordna så att även Norrbotten blir tillgängligt som GPX filer på samma sätt.
 Jämtland finns redan att ladda ned från natureit.se 
 
 ## Licens ##
+Copyright 2024 Outdoor Recreation Sverige
+All Rights Reserved Worldwide
+
+Licensed under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in
+compliance with the License.  You may obtain a copy of
+the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in
+writing, software distributed under the License is
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied.  See
+the License for the specific language governing
+permissions and limitations under the License.
+
 
 
--- scripts/kml2gpx.py
+++ scripts/kml2gpx.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python3
+#------------------------------------------------------------
+#   Copyright 2024 Outdoor Recreation Sverige
+#   All Rights Reserved Worldwide
+#
+#   Licensed under the Apache License, Version 2.0 (the
+#   "License"); you may not use this file except in
+#   compliance with the License.  You may obtain a copy of
+#   the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in
+#   writing, software distributed under the License is
+#   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#   CONDITIONS OF ANY KIND, either express or implied.  See
+#   the License for the specific language governing
+#   permissions and limitations under the License.
+#------------------------------------------------------------
+
 import os,sys
 import argparse
-#import xml
-#from xml.dom import minidom
-#import xml.etree.ElementTree as ET  
-#from xml.etree.ElementTree import Element, SubElement, ElementTree, tostring
 import subprocess
 import gpxpy
 import gpxpy.gpx
+from datetime import date
 
-
+today = date.today()
 
 parser = argparse.ArgumentParser(description='skapa en effektiv gpx fil från länsstyrelsens generade KML fil')
 
 parser.add_argument('-file',help='File to read', required=True)
 parser.add_argument('-output',help='Name of output file', required=True)
+parser.add_argument('-filter',help='Filter on name of area', required=False)
+parser.add_argument('-description',help='Description', required=False)
 args = parser.parse_args()
+
+
+
 filedata=open(args.file,"r")
 coordinates=""
 db={}
@@ -36,41 +57,57 @@ for line in filedata:
                 #print("Name="+name)
         elif "coordinate" in line:
             line=line.replace("<coordinates>",'')
-#            line=line.replace("0.0</coordinates>",'')
             line=line.replace("</coordinates>",'')
             line=line.replace("0.0 ",'')
             db[name]=line
-            #print("DBG:"+line)
 
 gpx = gpxpy.gpx.GPX()
+tdescription=args.description + """
+Används på egen risk.Filen skapad """ +str(today) + """ av Outdoor Recreation Sverige. info@fisketavling.nu""" 
 for n in db:
-    gpx_track = gpxpy.gpx.GPXTrack(name=n)
-    gpx.tracks.append(gpx_track)
-    # Segment
-    gpx_segment = gpxpy.gpx.GPXTrackSegment()
-    gpx_track.segments.append(gpx_segment)
-    c=db[n]
-    i=0;
-    pos=list(c.split(","))
-    #Remove last 0.0 to get an even list
-    pos.pop()
-    psize=len(pos)
-    for i in range(0,psize,2):
-        p=pos[i]
-        p=p.strip()
-        lon=pos[i]
-        lat=pos[i+1]
-        gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat,lon))
-
+    if (args.filter==None):
+        gpx_track = gpxpy.gpx.GPXTrack(name=n,description=tdescription)
+        gpx.tracks.append(gpx_track)
+        # Segment
+        gpx_segment = gpxpy.gpx.GPXTrackSegment()
+        gpx_track.segments.append(gpx_segment)
+        c=db[n]
+        i=0;
+        pos=list(c.split(","))
+        #Remove last 0.0 to get an even list
+        pos.pop()
+        psize=len(pos)
+        for i in range(0,psize,2):
+            p=pos[i]
+            p=p.strip()
+            lon=pos[i]
+            lat=pos[i+1]
+            gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat,lon))
+    elif (args.filter in n):
+        gpx_track = gpxpy.gpx.GPXTrack(name=n,description=tdescription)
+        gpx.tracks.append(gpx_track)
+        # Segment
+        gpx_segment = gpxpy.gpx.GPXTrackSegment()
+        gpx_track.segments.append(gpx_segment)
+        c=db[n]
+        i=0;
+        pos=list(c.split(","))
+        #Remove last 0.0 to get an even list
+        pos.pop()
+        psize=len(pos)
+        for i in range(0,psize,2):
+            p=pos[i]
+            p=p.strip()
+            lon=pos[i]
+            lat=pos[i+1]
+            gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat,lon))
         
 outfile=open(args.output,"w")
 for line in gpx.to_xml():
     outfile.write(line)
 outfile.close()
-    
 
-
-#print('Created GPX:', gpx.to_xml())
+print("Generated "+args.output)
 
 
 
